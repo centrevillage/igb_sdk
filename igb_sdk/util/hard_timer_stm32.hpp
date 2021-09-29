2ccc17448547f044ab8e579b2b3d9ec5ac8e72a1
centrevillage <centrevillage@gmail.com>
2021-09-29 16:44:28 +0900

    hard_timer accepts 16bit tim

--- igb_sdk/util/hard_timer_stm32.hpp
+++ igb_sdk/util/hard_timer_stm32.hpp
@@ -4,80 +4,31 @@
 #include <cmath>
 #include <array>
 #include <algorithm>
+#include <type_traits>
 #include <igb_stm32/periph/tim.hpp>
+#include <igb_util/tbl.hpp>
 
 namespace igb {
 namespace sdk {
 
-template<typename Tim32Cls /* 32bit tim class (TIM2 etc) */, uint8_t priority, uint32_t tim_base_clock>
+template<typename TimCls, typename count_t /* uint16_t or uint32_t */, uint8_t priority, uint32_t tim_base_clock>
 struct HardCcTimerStm32 {
-  constexpr static uint32_t sub_timer_count = 3;
-  constexpr static uint32_t adjust_tbl[32] = {
-// #!ruby
-//def gen_basic_euclid(k, n)
-//  if k > n
-//    k = n
-//  end
-//  pattern = [[1]] * k + [[0]] * (n - k)
-//  while k > 0
-//    cut = [k, pattern.length - k].min
-//    pattern = (0 ... cut).map {|i| pattern[i] + pattern[k + i]} + pattern[cut ... k] + pattern[(k+cut)...]
-//    k = cut
-//  end
-//  pattern.flatten
-//end
-//
-//(0..31).each do |fill|
-//  print <<-EOS
-//    0b#{gen_basic_euclid(fill, 32).join('')},
-//  EOS
-//end
-    0b00000000000000000000000000000000,
-    0b10000000000000000000000000000000,
-    0b10000000000000001000000000000000,
-    0b10000000000100000000010000000000,
-    0b10000000100000001000000010000000,
-    0b10000001000001000001000000100000,
-    0b10000010000100001000001000010000,
-    0b10000100010000100001000100001000,
-    0b10001000100010001000100010001000,
-    0b10001001000100010010001001000100,
-    0b10001001001001001000100100100100,
-    0b10010100100100100100100100100100,
-    0b10010100100101001001010010010100,
-    0b10010101001010010100101001010010,
-    0b10010101010010101001010101001010,
-    0b10010101010101010100101010101010,
-    0b10101010101010101010101010101010,
-    0b10110101010101010101101010101010,
-    0b10110101010110101011010101011010,
-    0b10110101011010110101101011010110,
-    0b10110101101101011011010110110101,
-    0b10110101101101101101101101101101,
-    0b10111011011011011011101101101101,
-    0b10111011011101110110111011011101,
-    0b10111011101110111011101110111011,
-    0b10111101110111101111011101111011,
-    0b10111110111101111011111011110111,
-    0b10111111011111011111011111101111,
-    0b10111111101111111011111110111111,
-    0b10111111111101111111110111111111,
-    0b10111111111111111011111111111111,
-    0b10111111111111111111111111111111,
-  };
+  static_assert(std::is_same<count_t, uint16_t>::value || std::is_same<count_t, uint32_t>::value, "count_t must be uint16_t or uint32_t");
 
-  Tim32Cls _tim;
+  constexpr static uint32_t sub_timer_count = 3;
+
+  TimCls _tim;
   struct CcState {
-    uint32_t interval_tick = 0;
+    count_t interval_tick = 0;
     float interval_tick_f = 0.0f;
-    uint32_t interval_tick_adj = 0;
-    uint32_t cc_value = 0;
+    count_t interval_tick_adj = 0;
+    count_t cc_value = 0;
     uint32_t update_count = 0;
     bool active = false;
     std::function<void(void)> on_capture = [](){}; // set by application
 
-    IGB_FAST_INLINE uint32_t getAdjestValue() {
-      return (adjust_tbl[interval_tick_adj] & (1UL << (update_count & 0x1F))) ? 1 : 0;
+    IGB_FAST_INLINE count_t getAdjestValue() {
+      return (euclid_tbl_32[interval_tick_adj] & (1UL << (update_count & 0x1F))) ? 1 : 0;
     }
     IGB_FAST_INLINE void nextCc() {
       cc_value += interval_tick + getAdjestValue();
@@ -97,10 +48,12 @@ struct HardCcTimerStm32 {
     _tim.count(0);
     setIntervalSec(cc1_interval_sec);
     _cc_states[0].on_capture = update_func;
+
+    const count_t period = (count_t)0xFFFFFFFF;
     _tim.init(
       igb::stm32::TimConf {
         .prescale = 0,
-        .period = 0xFFFFFFFF,
+        .period = period,
         .enable_it_cc1 = true,
         .interrupt_priority = priority
       }
@@ -134,7 +87,7 @@ struct HardCcTimerStm32 {
       setIntervalSec(sub_timer_idx+1, interval_sec);
     }
   }
-  IGB_FAST_INLINE void setIntervalTick(uint32_t interval_tick) { // general timer api
+  IGB_FAST_INLINE void setIntervalTick(count_t interval_tick) { // general timer api
     setIntervalTick(0, interval_tick);
   }
   IGB_FAST_INLINE void setIntervalTick(float interval_tick_f) { // general timer api
@@ -181,7 +134,7 @@ struct HardCcTimerStm32 {
 
   IGB_FAST_INLINE void _startTimer(uint8_t cc_idx) {
     auto& state = _cc_states[cc_idx];
-    uint32_t cc_value = _tim.count() + state.interval_tick;
+    count_t cc_value = _tim.count() + state.interval_tick;
     _setCcValue(cc_idx, cc_value);
     state.active = true;
     state.cc_value = cc_value;
@@ -237,7 +190,7 @@ struct HardCcTimerStm32 {
     }
   }
 
-  IGB_FAST_INLINE void setIntervalTick(uint8_t cc_idx, uint32_t interval_tick) { // specialized api
+  IGB_FAST_INLINE void setIntervalTick(uint8_t cc_idx, count_t interval_tick) { // specialized api
     auto& state = _cc_states[cc_idx];
     if (interval_tick > 0) {
       auto& state = _cc_states[0];
@@ -250,28 +203,28 @@ struct HardCcTimerStm32 {
   IGB_FAST_INLINE void setIntervalTick(uint8_t cc_idx, float interval_tick_f) { // specialized api
     auto& state = _cc_states[cc_idx];
     if (interval_tick_f > 0.0f) {
-      const auto tick_u32 = (uint32_t)interval_tick_f;
+      const auto tick_u32 = (count_t)interval_tick_f;
       state.interval_tick_f = interval_tick_f;
       state.interval_tick = tick_u32;
       const auto mod = interval_tick_f - (float)tick_u32;
       state.interval_tick_adj = std::clamp(
-          (uint32_t)(mod * 32.0f),
-          (uint32_t)0, (uint32_t) 31);
+          (count_t)(mod * 32.0f),
+          (count_t)0, (count_t) 31);
     }
   }
 
   IGB_FAST_INLINE void setSubTimerIntervalTick(uint8_t sub_timer_idx, float interval_tick_f) { // specialized timer api
     setIntervalTick(sub_timer_idx+1, interval_tick_f);
   }
-  IGB_FAST_INLINE void setSubTimerIntervalTick(uint8_t sub_timer_idx, uint32_t interval_tick) { // specialized timer api
+  IGB_FAST_INLINE void setSubTimerIntervalTick(uint8_t sub_timer_idx, count_t interval_tick) { // specialized timer api
     setIntervalTick(sub_timer_idx+1, interval_tick);
   }
 
-  IGB_FAST_INLINE uint32_t tick() { // specialized api
+  IGB_FAST_INLINE count_t tick() { // specialized api
     return _tim.count();
   }
 
-  IGB_FAST_INLINE void _setCcValue(uint8_t cc_idx, uint32_t value) {
+  IGB_FAST_INLINE void _setCcValue(uint8_t cc_idx, count_t value) {
     switch (cc_idx) {
       case 0:
         _tim.cc1Value(value);
---
+++ igb_util/tbl.hpp
@@ -0,0 +1,13 @@
+#ifndef IGB_UTIL_TBL_H
+#define IGB_UTIL_TBL_H
+
+#include <cstdint>
+
+namespace igb {
+
+extern uint32_t euclid_tbl_32[32];
+
+}
+
+
+#endif /* IGB_UTIL_TBL_H */
