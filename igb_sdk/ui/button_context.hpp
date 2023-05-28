b3a22a7989f8adfc031c6856863502de837d4196
centrevillage <centrevillage@gmail.com>
2023-05-28 09:18:33 +0900

    add button utilities

---
+++ igb_sdk/ui/button_context.hpp
@@ -0,0 +1,102 @@
+#ifndef IGB_SDK_UI_BUTTON_CONTEXT_H
+#define IGB_SDK_UI_BUTTON_CONTEXT_H
+
+#include <functional>
+#include <utility>
+#include <igb_sdk/base.hpp>
+#include <igb_sdk/ui/button.hpp>
+#include <igb_sdk/ui/button_history.hpp>
+#include <igb_util/conversion.hpp>
+
+namespace igb {
+namespace sdk {
+
+struct ButtonMethod {
+  constexpr static uint32_t makeStateBits(auto&& id, auto&&... rest) {
+    return ((uint32_t)1 << static_cast<uint32_t>(id)) | makeStateBits(rest...);
+  }
+
+  constexpr static uint32_t makeStateBits() {
+    return 0;
+  }
+
+  constexpr static bool isAllOff(uint32_t state_bits) {
+    return !state_bits;
+  }
+
+  constexpr static bool exactMatch(uint32_t state_bits, auto&& id, auto&&... rest) {
+    return state_bits == _exactMatch(id, rest...);
+  }
+
+  constexpr static uint32_t _exactMatch(auto&& id, auto&&... rest) {
+    return ((uint32_t)1 << static_cast<uint32_t>(id)) | _exactMatch(rest...);
+  }
+  constexpr static uint32_t _exactMatch() {
+    return 0;
+  }
+
+  constexpr static bool match(uint32_t state_bits, auto&& state, auto&&... rest) {
+    return (
+      state.second == !!(state_bits & ((uint32_t)1 << static_cast<uint32_t>(state.first)))
+        && match(rest...)
+    );
+  }
+
+  constexpr static bool match() {
+    return true;
+  }
+};
+
+
+template<typename AppBtnId, typename BtnCollection>
+struct ButtonContext {
+  std::function<void(AppBtnId, bool)> on_change = [](AppBtnId, bool){};
+  BtnCollection buttons;
+
+  typedef ButtonHistory<AppBtnId> AppBtnHistory;
+  typedef typename AppBtnHistory::Info AppBtnHistoryInfo;
+
+  AppBtnHistory history;
+  uint32_t state_bits = 0;
+  uint32_t prev_state_bits = 0;
+
+  void init() {
+    history.init();
+    buttons.init();
+  }
+
+  void process(uint32_t current_tick) {
+    history.process(current_tick);
+    buttons.process();
+
+    uint32_t new_bits = create_bits(
+      buttons, [](auto& btn) { return btn.isOn(); }
+    );
+
+    if (state_bits != new_bits) {
+      uint32_t diff_bits = state_bits ^ new_bits;
+      state_bits = new_bits;
+      for (size_t i = 0; i < buttons.size(); ++i) {
+        if (diff_bits & (1UL << i)) {
+          bool is_on = !!(new_bits & (1UL << i));
+          history.add(
+            AppBtnHistoryInfo {
+              .tick = current_tick,
+              .id = static_cast<AppBtnId>(i),
+              .state = (is_on ? (uint8_t)1 : (uint8_t)0)
+            }
+          );
+          if (on_change) {
+            on_change(static_cast<AppBtnId>(i), is_on);
+          }
+        }
+      }
+      prev_state_bits = state_bits;
+    }
+  }
+};
+
+}
+}
+
+#endif /* IGB_SDK_UI_BUTTON_CONTEXT_H */
---
+++ igb_sdk/ui/button_history.hpp
@@ -0,0 +1,67 @@
+#ifndef IGB_SDK_UI_BUTTON_HISTORY_H
+#define IGB_SDK_UI_BUTTON_HISTORY_H
+
+#include <igb_sdk/base.hpp>
+#include <igb_util/ring_buf.hpp>
+
+namespace igb {
+namespace sdk {
+
+template<typename BtnIdType, size_t history_size = 8>
+struct ButtonHistory {
+  struct Info {
+    uint32_t tick;
+    BtnIdType id;
+    uint8_t state; // 0 = leave, 1 = press, other = inactive
+  };
+  RingBuf<Info, history_size> history;
+
+  void init() {
+    for (size_t i = 0; i < history_size; ++i) {
+      auto& info = history._data[i];
+      info.state = 255;
+    }
+  }
+
+  void process(uint32_t current_tick) {
+    for (size_t i = 0; i < history_size; ++i) {
+      auto& info = history._data[i];
+      if (info.state != 255) {
+        if (current_tick - info.tick > 0x7FFFFFFFUL) {
+          // timeout
+          info.state = 255;
+        }
+      }
+    }
+  }
+
+  void add(const Info& info) {
+    history.add(info);
+  }
+
+  uint32_t findTapInterval(BtnIdType id, uint32_t long_press_limit_tick, uint32_t min_interval_tick, uint32_t max_interval_tick) const {
+    const auto& info0 = history.peekLast(0);
+    const auto& info1 = history.peekLast(1);
+    const auto& info2 = history.peekLast(2);
+    const auto& info3 = history.peekLast(3);
+    const uint32_t interval = info1.tick - info3.tick;
+    if (
+        info0.id == id && info0.state == 0 &&
+        info1.id == id && info1.state == 1 &&
+        info2.id == id && info2.state == 0 &&
+        info3.id == id && info3.state == 1 &&
+        min_interval_tick < interval && interval < max_interval_tick &&
+        (info2.tick - info3.tick) < (uint32_t)long_press_limit_tick &&
+        (info0.tick - info1.tick) < (uint32_t)long_press_limit_tick
+        ) {
+      return interval;
+    }
+
+    return 0; // not found
+  }
+};
+
+}
+}
+
+#endif /* IGB_SDK_UI_BUTTON_HISTORY_H */
