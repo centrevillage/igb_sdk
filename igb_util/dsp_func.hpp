3a07efdc95116e4c942cb46c15be941f757c76fa
centrevillage <centrevillage@gmail.com>
2023-06-01 10:25:01 +0900

    add dsp functions

--- igb_util/dsp_func.hpp
+++ igb_util/dsp_func.hpp
@@ -5,7 +5,51 @@
 
 namespace igb {
 
-IGB_FAST_INLINE float df_softclip(float v) {
+IGB_FAST_INLINE float df_saw(float x) {
+  return x;
+}
+
+IGB_FAST_INLINE float df_saw_biphase(float x) {
+  x -= (uint32_t)x;
+  return (x * 2.0f) - 1.0f;
+}
+
+IGB_FAST_INLINE float df_sqr(float x, float duty = 0.5f) {
+  if (x > duty) {
+    return 0.0f;
+  }
+  return 1.0f;
+}
+
+IGB_FAST_INLINE float df_sqr_biphase(float x, float duty = 0.5f) {
+  if (x > duty) {
+    return -1.0f;
+  }
+  return 1.0f;
+}
+
+IGB_FAST_INLINE float df_tri(float x, float duty = 0.5f) {
+  if (x > duty) {
+    const float w = 1.0f - duty;
+    const float a = x - duty;
+    return 1.0f - (a / w);
+  }
+  return x / duty;
+}
+
+IGB_FAST_INLINE float df_tri_biphase(float x) {
+  x -= (uint32_t)x;
+  if (x < -0.5f) {
+    return df_tri(((-x) - 0.5f) * 2.0f);
+  } else if (x < 0.0f) {
+    return -df_tri((-x) * 2.0f);
+  } else if (x < 0.5f) {
+    return df_tri(x * 2.0f);
+  }
+  return -df_tri((x - 0.5f) * 2.0f);
+}
+
+IGB_FAST_INLINE float df_softclip(float v /* -1.0 ~ 1.0 */) /* -> -1.0 ~ 1.0 */ {
   v *= 2.0f / 3.0f;
   if (v < -1.0f) {
     return -1.0f;
