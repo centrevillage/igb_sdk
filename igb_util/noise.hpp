35c904e4676a4e98c062cb044fa09421fabc3708
centrevillage <centrevillage@gmail.com>
2023-06-27 23:56:13 +0900

    add noise & pitch_shifter

---
+++ igb_util/noise.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <cmath>
+#include <array>
+#include <igb_util/random.hpp>
+#include <igb_util/dsp_tbl_func.hpp>
+
+namespace igb {
+
+struct PerlinNoise {
+  float sample_rate = 48000.0f;
+  float freq = 10.0f;
+  float delta = 0.0f;
+  float phase = 0.0f;
+
+  float a1 = 0.0f; /* 0.0 ~ 4.0 */
+  float a2 = 0.0f; /* 0.0 ~ 4.0 */
+
+  float a_max = 4.0f;
+
+  void init(float _sample_rate, float _freq) {
+    sample_rate = _sample_rate;
+    phase = 0.0f;
+    changeFreq(_freq);
+    a1 = (igb::random.getf() - 0.5f) * a_max * 2.0f;
+    a2 = (igb::random.getf() - 0.5f) * a_max * 2.0f;
+  }
+
+  void changeFreq(float _freq) {
+    freq = _freq;
+    delta = freq / sample_rate;
+  }
+
+  float process() {
+    phase += delta;
+    if (phase >= 1.0f) {
+      phase -= (uint32_t)phase;
+      a1 = a2;
+      a2 = (igb::random.getf() - 0.5f) * a_max * 2.0f;
+    }
+    float wavelet_v1 = igb::dsp_perlin_5order_fast(phase) * (phase * a1);
+    float phase2 = -1.0f + phase;
+    float wavelet_v2 = igb::dsp_perlin_5order_fast(phase2) * (phase2 * a2);
+    
+    return std::lerp(wavelet_v1, wavelet_v2, phase);
+  }
+};
+
+template<typename NoiseType = PerlinNoise, size_t order_size = 5>
+struct FractalNoise {
+  float sample_rate = 48000.0f;
+  float freq = 10.0f;
+  float decay = 0.5f;
+
+  std::array<NoiseType, order_size> noises;
+
+  void init(float _sample_rate, float _freq, float _decay = 0.5f) {
+    sample_rate = _sample_rate;
+    decay = _decay;
+    freq = _freq;
+    float freq_tmp = freq;
+    for (size_t i = 0; i < order_size; ++i) {
+      noises[i].init(sample_rate, freq_tmp);
+      freq_tmp *= 2.0f;
+    }
+  }
+
+  void changeFreq(float _freq) {
+    freq = _freq;
+    float freq_tmp = freq;
+    for (size_t i = 0; i < order_size; ++i) {
+      noises[i].changeFreq(freq_tmp);
+      freq_tmp *= 2.0f;
+    }
+  }
+
+  float process() {
+    float v = 0.0f;
+    float gain = 1.0f;
+    for (size_t i = 0; i < order_size; ++i) {
+      v += noises[i].process() * gain;
+      gain *= decay;
+    }
+    return v;
+  }
+};
+
+}
---
+++ igb_util/pitch_shifter.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cmath>
+#include <igb_util/delay_line.hpp>
+
+namespace igb {
+
+struct PitchShifter {
+  size_t window_size = 0;
+  size_t crossfade_duration = 0;
+
+  igb::DelayLine delay;
+
+  size_t write_pos = 0;
+  double pos_delta = 0.0f;
+  double pos_delta_cur = 0.0f;
+  double pos = 0.0f;
+  double pos_filter_coeff = 0.99f;
+  double zero_pos_filter_coeff = 0.99f;
+
+  constexpr static size_t spacer_size = 0;
+
+  void init(float* target_buf, size_t size /* greater than spacer_size + 1 */) {
+    delay.init(target_buf, size);
+    window_size = (size - spacer_size) / 2;
+    crossfade_duration = window_size / 4;
+    pos = (double)window_size / 2.0;
+  }
+
+  void shift(double semitone) {
+    pos_delta = 1.0 - std::pow(2.0, semitone / 12.0);
+  }
+
+  float process(float x) {
+    if (pos_delta == 0.0) {
+      pos = (1.0f - zero_pos_filter_coeff) * ((double)window_size / 2.0) + zero_pos_filter_coeff * pos;
+    } else {
+      //pos = (pos + pos_delta_cur + window_size) % window_size;
+      pos = std::fmod((pos + pos_delta_cur + (double)window_size), (double)window_size);
+    }
+    float d1 = delay.readF(pos);
+    float d2 = delay.readF(pos + (double)window_size);
+    pos_delta_cur = (1.0 - pos_filter_coeff) * pos_delta + pos_filter_coeff * pos_delta_cur;
+    float rate = std::min(pos / (double)crossfade_duration, 1.0);
+    float y = d1 * rate + d2 * (1.0f - rate);
+
+    delay.write(x);
+
+    return y;
+  }
+};
+
+}
