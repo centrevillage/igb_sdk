bd5f1a426a57c412d657a6f149d439cfa6cc77dd
centrevillage <centrevillage@gmail.com>
2024-05-28 08:59:52 +0900

    constexpr

--- igb_util/cast.hpp
+++ igb_util/cast.hpp
@@ -3,12 +3,12 @@
 #include <type_traits>
 
 namespace igb {
-  template <typename T> static inline T as(auto&& value) {
+  template <typename T> constexpr static T as(auto&& value) {
     return static_cast<T>(value);
   }
 
   // TODO: コンパイルエラーになるので要修正
-  template <typename T> static inline auto enum_cast(const T& v) {
+  template <typename T> constexpr static auto enum_cast(const T& v) {
     return std::underlying_type<T>::type(v);
   }
 }
