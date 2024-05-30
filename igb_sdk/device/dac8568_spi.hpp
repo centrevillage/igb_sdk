7a2084c69038bd05f07dc92997830d25fc89cb97
centrevillage <centrevillage@gmail.com>
2024-05-30 10:16:36 +0900

    add dac8568_spi

---
+++ igb_sdk/device/dac8568_spi.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <igb_sdk/base.hpp>
+#include <functional>
+#include <igb_util/macro.hpp>
+#include <igb_util/cast.hpp>
+#include <igb_util/null_functor.hpp>
+
+namespace igb {
+namespace sdk {
+
+template<typename SPI_TYPE, typename GPIO_PIN_TYPE, typename WAIT_FUNC = NullFunctor>
+struct Dac8568SPI {
+  // public attributes {{{
+  SPI_TYPE spi;
+  GPIO_PIN_TYPE cs_pin; // = sync_pin
+  // }}}
+
+  WAIT_FUNC _wait_func;
+
+  enum class LoadCmd : uint8_t {
+    noLoad = 0,
+    single = 0x03,
+    all = 0x02,
+  };
+
+  void init() {
+    cs_pin.enable();
+    cs_pin.initOutputDefault();
+    cs_pin.high();
+  }
+
+  IGB_FAST_INLINE void _writeByte(uint8_t byte) {
+    volatile uint8_t tmp IGB_UNUSED = spi.transferU8sync(byte, _wait_func);
+  }
+
+  IGB_FAST_INLINE void writeData(uint8_t ch /* 0 ~ 7 */, uint16_t data, LoadCmd load_cmd = LoadCmd::single) {
+    cs_pin.low();
+    _writeByte(as<uint8_t>(load_cmd));
+    _writeByte(ch << 12 | (data >> 12));
+    _writeByte((data >> 4) & 0xFF);
+    _writeByte((data << 4) & 0xF0);
+    cs_pin.high();
+  }
+};
+
+}
+}
