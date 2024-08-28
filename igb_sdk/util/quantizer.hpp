22f43e9520dcba5ccebaf170460c2f8263b149dc
centrevillage <centrevillage@gmail.com>
2024-08-28 10:04:11 +0900

    add weighted scale quantizer

--- igb_sdk/util/quantizer.hpp
+++ igb_sdk/util/quantizer.hpp
@@ -7,6 +7,7 @@
 namespace igb {
 namespace sdk {
 
+// Neighbor Quantizer
 struct ScaleQuantizer {
   uint8_t max_octave = 10;
 
@@ -22,7 +23,7 @@ struct ScaleQuantizer {
     }
   }
 
-  uint8_t quantize(uint16_t note) const noexcept {
+  uint8_t quantize(uint16_t note /* 0 ~ 119 */) const noexcept {
     return std::clamp((int16_t)(note + _scale_map[note % 12]), _min_note, _max_note);
   }
 
@@ -66,6 +67,91 @@ struct ScaleQuantizer {
         } else {
           found_127 = 1;
         }
+      }
+    }
+  }
+};
+
+// Weighted Quantizer
+template<uint16_t resolution_rate = 8 /* 1 ~ 43 */>
+struct WeightedScaleQuantizer {
+  uint8_t max_octave = 10;
+
+  uint16_t _scale_bit = 0x0FFF;
+  std::array<uint8_t, 12> _priorities; // 0~127
+  constexpr static uint16_t _scale_map_size = 12 * resolution_rate;
+  std::array<int16_t, _scale_map_size> _scale_map;
+  int16_t _max_note = (max_octave * 12 * resolution_rate) - 1;
+  int16_t _min_note = 0;
+
+  void updateScale(const std::array<uint8_t, 12>& priorities) {
+    bool is_changed = false;
+    for (uint8_t i = 0; i < 12; ++i) {
+      if (_priorities[i] != priorities[i]) {
+        is_changed = true;
+        break;
+      }
+    }
+    if (is_changed) {
+      uint16_t new_scale_bit = 0; 
+      for (uint16_t i = 0; i < 12; ++i) {
+        new_scale_bit |= ((priorities[i] > 0 ? 1 : 0) << i);
+      }
+      if (new_scale_bit) { // avoid illegal scale
+        _priorities = priorities;
+        _scale_bit = new_scale_bit;
+        _reconstruct();
+      }
+    }
+  }
+
+  uint8_t quantize(uint16_t note /* 0 ~ 12 * 10 * resolution_rate  */) const noexcept {
+    return std::clamp((int16_t)(note + _scale_map[note % _scale_map_size]), _min_note, _max_note);
+  }
+
+  void _reconstruct() {
+    // reconstruct _scale_map
+    // don't use float type for no-fpu mcu 
+    
+    uint16_t priority_sum = 0;
+    for (uint8_t i = 0; i < 12; ++i) {
+      priority_sum += _priorities[i];
+    }
+    std::array<uint16_t, 12> key_range_sizes;
+    uint16_t assigned_size = 0;
+    for (uint16_t i = 0; i < 12; ++i) {
+      if (_scale_bit & (1 << i)) {
+        key_range_sizes[i] = _priorities[i] * _scale_map_size / priority_sum;
+        assigned_size += key_range_sizes[i];
+      } else {
+        key_range_sizes[i] = 0;
+      }
+    }
+    uint16_t remain_size = _scale_map_size - assigned_size;
+    // TODO: optimizing by sort algorithm
+    for (; remain_size > 0; --remain_size) {
+      uint16_t min_size = _scale_map_size;
+      int8_t min_idx = -1;
+      for (int8_t i = 0; i < 12; ++i) {
+        if (_scale_bit & (1 << i)) {
+          if (key_range_sizes[i] < min_size) {
+            min_size = key_range_sizes[i];
+            min_idx = i;
+          }
+        }
+      }
+      if (min_idx >= 0) {
+        key_range_sizes[min_idx] += 1;
+      }
+    }
+
+    uint16_t scale_map_idx = 0;
+    for (int16_t key = 0; key < 12; ++key) {
+      uint16_t key_range_size = key_range_sizes[key];
+      int16_t target_key_value = key * resolution_rate;
+      for (uint16_t j = 0; j < key_range_size; ++j) {
+        _scale_map[scale_map_idx] = -(scale_map_idx - target_key_value);
+        ++scale_map_idx;
       }
     }
   }
--- test/igb_sdk/src/test_quantizer.cpp
+++ test/igb_sdk/src/test_quantizer.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <bitset>
+#include <array>
 
 using namespace igb;
 using namespace igb::sdk;
@@ -61,4 +62,79 @@ TEST_CASE("ScaleQuantizer::updateScale", "[scale_bit]") {
     REQUIRE((int)quantizer.quantize(11) == 14);
   }
 
+}
+
+
+TEST_CASE("WeightedScaleQuantizer::updateScale", "[priorities]") {
+  constexpr static size_t resolution = 8;
+  WeightedScaleQuantizer<resolution> quantizer;
+
+  SECTION("scale C(+)DE(+)F(-)A" ) {
+    std::array<uint8_t, 12> priorities = {
+      127, // C
+      0, // C#
+      84, // D 
+      0, // D#
+      127, // E
+      43, // F
+      0, // F#
+      0, // G
+      0, // G#
+      84, // A
+      0, // A#
+      0 // B
+    };
+
+    quantizer.updateScale(priorities);
+    std::cout << "WeightedScaleQuantizer (C(+)DE(+)F(-)A):" << std::endl;
+    std::cout << "scale_map = ";
+    for (const auto& e : quantizer._scale_map) {
+      std::cout << (int)e << ",";
+    }
+    std::cout << std::endl;
+
+    REQUIRE((int)quantizer.quantize(0 * resolution) == 0 * resolution); // C
+    REQUIRE((int)quantizer.quantize(1 * resolution) == 0 * resolution);
+    REQUIRE((int)quantizer.quantize(2 * resolution) == 0 * resolution);
+    REQUIRE((int)quantizer.quantize(3 * resolution) == 0 * resolution);
+    REQUIRE((int)quantizer.quantize(4 * resolution) == 2 * resolution); // D
+    REQUIRE((int)quantizer.quantize(5 * resolution) == 2 * resolution); // D
+    REQUIRE((int)quantizer.quantize(6 * resolution) == 4 * resolution); // E
+    REQUIRE((int)quantizer.quantize(7 * resolution) == 4 * resolution);
+    REQUIRE((int)quantizer.quantize(8 * resolution) == 4 * resolution);
+    REQUIRE((int)quantizer.quantize(9 * resolution) == 5 * resolution); // F
+    REQUIRE((int)quantizer.quantize(10 * resolution) == 9 * resolution); // A
+    REQUIRE((int)quantizer.quantize(11 * resolution) == 9 * resolution);
+    REQUIRE((int)quantizer.quantize(12 * resolution - 1) == 9 * resolution);
+    REQUIRE((int)quantizer.quantize(12 * resolution) == 12 * resolution); // C(octave up)
+  }
+
+  SECTION( "scale only E" ) {
+    std::array<uint8_t, 12> priorities = {
+      0, // C
+      0, // C#
+      0, // D 
+      0, // D#
+      84, // E
+      0, // F
+      0, // F#
+      0, // G
+      0, // G#
+      0, // A
+      0, // A#
+      0 // B
+    };
+
+    quantizer.updateScale(priorities);
+    std::cout << "WeightedScaleQuantizer (E):" << std::endl;
+    std::cout << "scale_map = ";
+    for (const auto& e : quantizer._scale_map) {
+      std::cout << (int)e << ",";
+    }
+    std::cout << std::endl;
+
+    for (uint16_t i = 0; i < 12; ++i) {
+      REQUIRE((int)quantizer.quantize(i * resolution) == 4 * resolution); // E
+    }
+  }
 }
